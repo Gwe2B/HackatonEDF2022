e38261d14daf3d48f7c1e82cd7868164aa7e6e59
marioncalpe <marioncalpe2a@gmail.com>
2022-12-15 10:45:07 +0100

    initialisation de pygame et de la fenetre de jeu

---
+++ main.py
@@ -0,0 +1,11 @@
+#importation du module pygame
+import pygame
+
+#intialition pygame
+pygame.init()
+
+#création de la fenetre du jeu 
+size_x = 800
+size_y = 600
+screen = pygame.display.set_mode((size_x,size_y))
+
