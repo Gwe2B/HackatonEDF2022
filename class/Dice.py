d181f23b05c1bed381acde8e81d52381dceb6b2f
Amineenima <aminemaourid1995@gmail.com>
2022-12-15 13:26:52 +0100

    adding the dice logic and request manager for api

--- class/Dice.py
+++ class/Dice.py
@@ -4,6 +4,8 @@ class Dice:
     Author  : Amine
     Version : 1
     Date    : 15/12/2022
+    Description : this class handles generating a random number for dices 
+    deplacement dice and charge dice
     """
     
     def __init__(self, type):
---
+++ class/RequestManager.py
@@ -0,0 +1,24 @@
+from model.api_fields import ApiField
+import requests
+
+class RequestManager:
+    """
+    Author  : Amine
+    Version : 1
+    Date    : 15/12/2022
+    Description : this class handles the request to API and returning simple exploitable data as tha state for charge and data
+
+    """
+    def __init__(self, url):
+        # this class handles interacting with the API and returning usefel data 
+        self.records:list = []
+        response = requests.get(url)
+        if(response.status_code == 200):
+            for record in response.json()['records']:
+                self.records.append(ApiField(**(record['fields'])))
+        else :
+            return response.status_code
+        
+
+
+        
